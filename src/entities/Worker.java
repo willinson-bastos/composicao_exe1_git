56afd057964c143b882e1a6b6f639a12bf3da2c0
Willinson Bastos <willinson.bastos@gmail.com>
2020-07-05 10:08:34 -0300

    criados constructors, getters, setters e metodos de add & remove contracts

--- src/entities/Worker.java
+++ src/entities/Worker.java
@@ -1,5 +1,6 @@
 package entities;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import entities.enums.WorkerLevel;
@@ -11,5 +12,66 @@ public class Worker {
 	private Double baseSalary;
 	
 	private Department department;
-	private List<HourContract> contracts;
+	private List<HourContract> contracts = new ArrayList<HourContract>();
+	
+	//constructors
+	public Worker() {
+		
+	}
+
+	public Worker(String name, WorkerLevel level, Double baseSalary, Department department) {
+		this.name = name;
+		this.level = level;
+		this.baseSalary = baseSalary;
+		this.department = department;
+	}
+	
+	//getters & setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public WorkerLevel getLevel() {
+		return level;
+	}
+
+	public void setLevel(WorkerLevel level) {
+		this.level = level;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public List<HourContract> getContracts() {
+		return contracts;
+	}
+
+	
+	//methods
+	public void addContract(HourContract contract) {
+		contracts.add(contract);
+	}
+	public void removeContract(HourContract contract) {
+		contracts.remove(contract);
+	}
+	
+	
+	
 }
